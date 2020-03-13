ee6621780fec3bac4bd6b560c238f1653af2f500
Agnaldo <agnaldo.souto.junior@accenture.com>
2020-03-13 15:21:51 -0300

    tela de registro iniciada

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Loader from "react-loader";
+import logo from "../assets/images/logo.jpg";
+import "../styles/pages/Register.css";
 
 const Register = () => {
   return (
-    <div style={{ backgroundColor: "#141414", height: "100%" }}>REGISTER</div>
+    <div className="main_register_container">
+      <div className="seja_bem_vindo">
+        <img className="img_logo" src={logo} />
+        <b className="texto_bem_vindo">Seja bem vindo!</b>
+      </div>
+
+      <div className="form_register">
+        <b className='cadastre_se'>CADASTRE-SE</b>
+        <input className='inputs' placeholder='Nome Completo'></input>
+        <input className='inputs' placeholder='Curso'></input>
+        <input className='inputs' placeholder='Email'></input>
+        <input style={{marginTop: '30px'}} className='inputs' placeholder='Senha'></input>
+        <input className='inputs' placeholder='Repita sua senha'></input>
+
+        <button className='register_button'>FINALIZAR CADASTRO</button>
+      </div>
+    </div>
   );
 };
 
