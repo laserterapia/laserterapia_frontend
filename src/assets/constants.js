fcba68a7d4c5e47c98c0d0daa4a1779bd040e361
AgnaldoCC <agnaldo.junior@ccc.ufcg.edu.br>
2020-10-25 13:19:05 -0300

    Ajuste url back

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3003"
+export const BASE_URL = "https://laserterapia-backend.herokuapp.com/"
 export const LOGIN_SUCCESS = 'login_success'
 export const REGISTER_SUCCESS = 'register_success'
 export const URL_FRONT = "http://localhost:3000"
